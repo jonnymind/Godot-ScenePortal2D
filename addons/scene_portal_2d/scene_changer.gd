d4818df3c7d80523e9054417cab771c58336a74f
Giancarlo Niccolai <gc@niccolai.cc>
2020-07-03 00:00:38 +0100

    Support for player teleport, intra-scene portals and portal loops.

--- addons/scene_portal_2d/scene_changer.gd
+++ addons/scene_portal_2d/scene_changer.gd
@@ -9,18 +9,22 @@ signal new_scene_loaded(scene)
 signal completed
 "After scene change is complete"
 
+export(String) var group_to_move = "Player"
+"When changing scene all the objects in this group will be moved to the target portal."
+export(String) var group_to_hook = "PlayerHook"
+"An group containing an empty node where the player will be connected when traversing `teleport` portals."
+
 var _scene_stack = []
 var _scene_db = {}
-var _target_position = null
-var _loading = false
 
 #################################################
 # Public interface
 #################################################
 	
-func change(path, transition=null, target_portal=-1, keep_current=false) -> void:
+func change(path, transition=null, target_portal=-1, keep_current=false, player=null) -> void:
 	"""
 		Move the scene to the target resource.
+		`path`: the res:// path to the scene to load, or null for same scene.
 		`transition`: A transition object exposing `enter(source, target)` and 
 					  `leave(source_target)`.
 		`target_portal`: if > 0, will move all the objects in `Player` group
@@ -29,35 +33,48 @@ func change(path, transition=null, target_portal=-1, keep_current=false) -> void
 		`keep_current`: if true, the current scene will be saved, and its state
 						will be restored when changed back.
 	"""
-	_change_instance(_get_resource(path), transition, target_portal, keep_current)
+	var status = _make_status(
+			_get_resource(path), 
+			transition, 
+			target_portal, 
+			keep_current,
+			player)
+	_change_instance(status)
+	
 
 
-func push(path, transition=null, target_portal=-1, keep_current=false, source_portal=null) -> void:
+func push(path, source_portal, transition=null, target_portal=-1, keep_current=false, player=null) -> void:
 	"""
 		Move the scene to the target resource, recording the previous scene.
+		`path`: the res:// path to the scene to load, or null for same scene.
 		`transition`: A transition object exposing `enter(source, target)` and 
 					  `leave(source_target)`.
-		`target_portal`: if > 0, will move all the objects in `Player` group
+		`target_portal_id`: if > 0, will move all the objects in `Player` group
 						 to the first portal with that ID found in the target
 						 scene
 		`keep_current`: if true, the current scene will be saved, and its state
 						will be restored when changed back.
+		`source_portal_id`: the ID of the source portal
 		If keep_current is not true, when pop() is invoked the current scene 
 		status will *not* be restored, but reloaded and re-instantiated. 
-		
 		Calls to `push()` and `pop()` can be interleaved with `change()`,
 		but if a scene is saved by either of them (using keep_current=true), 
 		it will be restored when referenced by any other function.
 	"""
-	var current = get_tree().current_scene
-	var position
-	if source_portal:
-		position = source_portal.global_position
-	_scene_stack.append([current.filename, position])
-	_change_instance(_get_resource(path), transition, target_portal, keep_current)
+	var current
+	# Handle local enters
+	if path:
+		current = get_tree().current_scene.filename
+	_scene_stack.append([current, source_portal])
+	_change_instance(_make_status(
+			_get_resource(path), 
+			transition, 
+			target_portal, 
+			keep_current, 
+			player))
 
 
-func pop(transition=null, target_portal=-1, keep_current=false) -> void:
+func pop(transition=null, target_portal=-1, keep_current=false, player=null) -> void:
 	"""
 		Move the scene to the last previously pushed one.
 		`transition`: A transition object exposing `enter(source, target)` and 
@@ -66,12 +83,18 @@ func pop(transition=null, target_portal=-1, keep_current=false) -> void:
 						 to the first portal with that ID found in the target
 						 scene
 		`keep_current`: if true, the current scene will be saved, and its state
-						will be restored when changed back. 
+						will be restored when changed back.
 	"""
 	var target = _scene_stack.pop_back()
 	assert(target != null)
-	_target_position = target[1]
-	change(target[0], transition, target_portal, keep_current)
+	if target_portal <= 0:
+		target_portal = target[1]
+	_change_instance(_make_status(
+			_get_resource(target[0]), 
+			transition, 
+			target_portal, 
+			keep_current,
+			player))
 
 
 func clear_area() -> void:
@@ -93,32 +116,38 @@ func get_default_transition() -> Node:
 		return ctrl.get_transition()
 	return null
 
-func is_loading() -> bool:
-	"Safe way to know if the scene loader is currently loading"
-	return _loading
-	
+
 #########################################################
 # Private
 #########################################################
 
-func _change_instance(instance, transition=null, target_portal=-1, keep_current=false) -> void:
-	if keep_current:
+func _change_instance(status) -> void:
+	if status.keep:
 		var current = get_tree().current_scene
 		var node_name = current.filename
 		if node_name == null:
 			node_name = current.name
 		_scene_db[node_name] = current
+	call_deferred("_load_next_scene", status)
+
+
+func _make_status(instance, transition, target_portal, keep_current, player):
+	# force non-teleport mode if this is an intra-scene portal
+	if not instance:
+		player = null
 	var transition_status = {
-		"tree": get_tree(),
 		"scene": instance,
 		"transition": transition,
 		"portal": target_portal,
-		"keep": keep_current
+		"keep": keep_current,
+		"teleported_player": player
 	}
-	call_deferred("_load_next_scene", transition_status)
+	return transition_status
 
 
 func _get_resource(path) -> Node:
+	if not path:
+		return null
 	if _scene_db.has(path):
 		return _scene_db[path]
 	var resource = load(path)
@@ -127,8 +156,15 @@ func _get_resource(path) -> Node:
 
 
 func _load_next_scene(status) ->void:
-	_loading = true
-	status.current = status.tree.current_scene
+	status.current = get_tree().current_scene
+	var player = status.teleported_player
+	var saved_layer
+	if player:
+		# A teleported player has pending physics from the old scene,
+		# we need to make it untouchable and let the phisics loop to complete.
+		saved_layer = player.collision_layer
+		player.collision_layer = 0
+		
 	emit_signal("started", status.scene)
 	if status.transition != null and status.transition.will_animate_leave():
 		add_child(status.transition)
@@ -137,56 +173,87 @@ func _load_next_scene(status) ->void:
 	emit_signal("old_scene_transitioned")
 	
 	# The workhorse of the scene changer
-	_replace_scene(status.tree, status.scene)
-	_move_player(status.portal)
+	if status.scene:
+		_replace_scene(status)
+	if status.teleported_player:
+		_hook_player_in_scene(status)
+	else:
+		_move_player(status)
 	emit_signal("new_scene_loaded")
 	
 	if status.transition != null and status.transition.will_animate_enter():
 		status.transition.enter(status)
 		yield(status.transition, "transitioned_in")
+	elif player:
+		# We need to give the physics the chance to recalculate in the new scene,
+		# hence we yield, but want to be called back immediately
+		yield(get_tree().create_timer(0.0001), "timeout")
 
 	if not status.keep:
 		status.current.queue_free()
-	_loading = false
 	if status.transition:
 		remove_child(status.transition)
 		status.transition.queue_free()
+	if player:
+		player.collision_layer = saved_layer
 	emit_signal("completed")
 
 
-func _replace_scene(tree, scene) -> void:
+func _replace_scene(status) -> void:
 	# Save the tree, as get_tree() will be lost once we remove ourselves
+	var scene = status.scene
+	var tree = get_tree()
 	var current = tree.current_scene
+	# Eventually remove the player
+	if status.teleported_player:
+		status.teleported_player.get_parent().remove_child(status.teleported_player)
 	tree.root.remove_child(current)
 	tree.root.add_child(scene)
 	# Need to update the current scene
 	tree.current_scene = scene
 
 
-func _move_player(portal_id) -> void:
-	if portal_id < 0 or (portal_id == 0 and _target_position == null):
+func _move_player(status) -> void:
+	var portal_id = status.portal
+	if portal_id < 0:
 		return
 
-	var players = get_tree().get_nodes_in_group("Player")
+	var players = get_tree().get_nodes_in_group(group_to_move)
 	if players.empty():
 		return
 
-	var portal_pos
-	var portal_facing = "None"
-	if portal_id == 0:
-		portal_pos = _target_position
-		_target_position = null
-	else:
+	var target_portal = _find_portal_in_scene(portal_id)
+	if target_portal:
+		for player in players:
+			_move_player_to_portal(player, target_portal)
+		
+
+func _find_portal_in_scene(portal_id):
+	if portal_id > 0:
 		var portals = get_tree().get_nodes_in_group("Portal")	
 		for portal in portals:
-			if portal.portalId == portal_id:
-				portal_pos = portal.global_position
-				portal_facing = portal.facing
-				break
-		if not portal_pos:
-			return
-	
-	for player in players:
-		player.global_position = portal_pos
-		if portal_facing != "None" and player.has_method("set_portal_facing"):
-			player.set_portal_facing(portal_facing)
+			if portal.portal_id == portal_id:
+				return portal
+	return null
+
+
+func _move_player_to_portal(body, portal):
+	portal.exiting(body)
+	body.global_position = portal.global_position
+	if portal.facing != "None" and body.has_method("set_portal_facing"):
+		body.set_portal_facing(portal.facing)
+
+func _hook_player_in_scene(status):
+	var portal = _find_portal_in_scene(status.portal)
+	var player = status.teleported_player
+	var hooks = get_tree().get_nodes_in_group(group_to_hook)
+	if hooks.size() != 1:
+		print("Need to have exactly one hook object in the target scene of a teleport in ", get_tree().current_scene.filename)
+		assert(false)
+		return
+	if portal:
+		portal.exiting(player)
+		player.global_position = portal.global_position
+	else:
+		player.global_position = hooks[0].global_position
+	hooks[0].add_child(player)
--- addons/scene_portal_2d/scene_portal_2d.gd
+++ addons/scene_portal_2d/scene_portal_2d.gd
@@ -12,30 +12,38 @@ system. SceneChanger can be used by other objects, or directly from the scripts,
 to invoke scene change programmatically.
 
 Scripts variables:
-	- destination: a string representing the target scene resource.
+	- destination: a string representing the target scene resource. If left unset.
+			this will be a intra-scene portal, moving the player to 
+			another area in the same scene.
+	- transition: Node path to a transition contol object.
+	- use_default_transition: if true, use the default transition in the AutoLoad
+			SceneController.
 	- type: One of the following:
 		- CHANGE: Just move to the next scene.
 		- ENTER: Pushes the current scene so that the next portal of type EXIT  
-				 will return to this scene.
+			 will return to this scene.
 		- EXIT: Ignores the destination and ID parameters, and restores
-				the scene and position from where the last ENTER portal was 
-				traversed. 
+			the scene and position from where the last ENTER portal was 
+			traversed. 
 		- EXIT_TO: Ignores the destination parameter, but not the ID; restores
-				the scenefrom where the last ENTER portal was 
-				traversed, but will connect to the portal with the same ID. 
+			the scenefrom where the last ENTER portal was 
+			traversed, but will connect to the portal with the same ID. 
 		- EXIT_ONLY: This portal can't be entered; they are only used to place 
-				the player in this scene automatically.
+			the player in this scene automatically.
 	- facing: The direction the player should face when landing on this portal.
-				If the player object exposes a 'set_portal_facing' method, it
-				will be called with this value.
-	- areaMode: If clicked, this scene is saved, and will be resumed when 
-				 re-entered from another area portal. If not, the scene status 
-				 will be discarded, and possibly re-instantiated on re-enter.
-	- portalId: moves the objects in the `Player` group to the portal having the
-				 same ID in the target scene. If the ID is 0, this feature is
-				 disabled (and the player appears at its own origin).
-	- transition: Type of transition.
-	- transitionTime: Lenght of the transition in seconds. 
+			If the player object exposes a 'set_portal_facing' method, it
+			will be called with this value.
+	- area_mode: If clicked, this scene is saved, and will be resumed when 
+			re-entered from another area portal. If not, the scene status 
+			will be discarded, and possibly re-instantiated on re-enter.
+	- portal_id: Unique identifier of this portal in this scene
+	- target: the player object will be moved to the portal having this ID in the
+			target scene. If zero, the player object will not be moved, and will
+			appear in its origin (or previous) position.
+	- teleport: if true, will move the player object instance to the target 
+			scene. The player will be attached to a node in the group 
+			PlayerHook. Exactly one player hooks must be present in scenes
+			target of teleports. This setting is ignored for intra-scene portals.
 """
 signal portal_entered(portal, body)
 signal portal_exited(portal, body)
@@ -45,44 +53,53 @@ enum FacingType {NONE, LEFT, UP, RIGHT, DOWN}
 
 export(String, FILE, "*.tscn") var destination
 export(NodePath) var transition
-export(bool) var useDefaultTransition
+export(bool) var use_default_transition
 export(PortalType) var type
-export(bool) var areaMode = true
-export(int) var portalId = 1
+export(bool) var area_mode = true
+export(int) var portal_id = 1
+export(int) var target = 1
+export(bool) var teleport = false
 export(String, "None", "Left", "Up", "Right", "Down") var facing = "None"
 
 var _active = true
+var _exiting = null
+
+func exiting(body) -> void:
+	_exiting = body
+	_active = false
+
 
 func _on_ScenePortal_body_entered(body):
-	if SceneChanger.is_loading():
-		_active = false
-		return
-	
 	if type == PortalType.EXIT_ONLY or not _active:
 		return
 	
-	_active = false
-	var trInstance
-	if useDefaultTransition:
-		trInstance = SceneChanger.get_default_transition()
+	if destination:
+		_active = false
+	
+	var trans_inst
+	if use_default_transition:
+		trans_inst = SceneChanger.get_default_transition()
 	elif transition != "":
-		trInstance = get_node(transition).get_transition()
-
+		trans_inst = get_node(transition).get_transition() 
+	
+	var player
+	if teleport:
+		player = body
 	emit_signal("portal_entered", self, body)
 	match type:
 		PortalType.CHANGE:
-			assert(destination != null)
-			SceneChanger.change(destination, trInstance, portalId, areaMode)
+			SceneChanger.change(destination, trans_inst, target, area_mode, player)
 		PortalType.ENTER:
-			assert(destination != null)
-			SceneChanger.push(destination, trInstance, portalId, areaMode, self)
+			SceneChanger.push(destination, portal_id, trans_inst, target, area_mode, player)
 		PortalType.EXIT:
-			SceneChanger.pop(trInstance, 0, areaMode)
+			SceneChanger.pop(trans_inst, 0, area_mode, player)
 		PortalType.EXIT_TO:
-			SceneChanger.pop(trInstance, portalId, areaMode)
+			SceneChanger.pop(trans_inst, target, area_mode, player)
 
 
 func _on_ScenePortal_body_exited(body):
-	if not SceneChanger.is_loading():
+	if _exiting == body:
 		_active = true
+		_exiting = null
 		emit_signal("portal_exited")
+
